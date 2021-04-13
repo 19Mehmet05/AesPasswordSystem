f64d5ce2496375f7642b27c51d5471135da9244a
Mehmet1905 <mehmet.buykbayram@gmail.com>
2021-04-13 10:58:03 +0300

    Dll Dosyası oluşturuldu

---
+++ ArunaObfuscationSystem.Dll/Obfuscation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AesPasswordSystem;
+using ArunaObfuscationSystem;
+
+namespace ArunaObfuscationSystem.Dll
+{
+    public  class  Obfuscation
+    {
+       static  ArunaObfuscation aruna = new ArunaObfuscation();
+        public static string ArunaObfuscation(string text)
+        {
+            var s = aruna.Obfuscate(text);
+            text = s;
+            return text;
+        }
+        public static string ArunaSolution(string text)
+        {    
+            var c = aruna.Enlighten(text);
+            text = c;
+            return text;
+        }
+    }
+}
